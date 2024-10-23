ff2b4fc8e047b9b3c17e8bb3dc1fdd7c025979c8
shojisjis <steve040315@gmail.com>
2024-10-23 17:52:20 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,7 +3,9 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()  # wait for client
+    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    conn.close()
 
 
 if __name__ == "__main__":
