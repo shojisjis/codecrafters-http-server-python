5190e7cfb40bce05e64c0f80ccc272d040e1e331
shojisjis <steve040315@gmail.com>
2024-10-23 18:12:52 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,9 +3,24 @@ import socket  # noqa: F401
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
-    conn.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-    conn.close()
+    
+    while True:
+        conn, addr = server_socket.accept()  # 클라이언트 대기
+        
+        request = conn.recv(1024).decode('utf-8')
+        print(request)
+        
+        # HTTP 요청 파싱
+        request_line = request.split('\n')[0]
+        method, target, _ = request_line.split(' ')
+        
+        if method == 'GET' and target == '/':
+            response = b"HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        
+        conn.sendall(response)
+        conn.close()
 
 
 if __name__ == "__main__":
