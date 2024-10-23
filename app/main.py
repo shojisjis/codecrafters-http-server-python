7f8e387273a69590bf78a98bbc8876b745780948
shojisjis <steve040315@gmail.com>
2024-10-23 19:32:23 +0900

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,8 +14,15 @@ def main():
         request_line = request.split('\n')[0]
         method, target, _ = request_line.split(' ')
         
-        if method == 'GET' and target == '/':
-            response = b"HTTP/1.1 200 OK\r\n\r\n"
+        if method == 'GET':
+            if target == '/':
+                response = b"HTTP/1.1 200 OK\r\n\r\n"
+            elif target.startswith('/echo/'):
+                echo_string = target[6:]  # '/echo/' 이후의 문자열 추출
+                response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_string)}\r\n\r\n{echo_string}"
+                response = response.encode()
+            else:
+                response = b"HTTP/1.1 404 Not Found\r\n\r\n"
         else:
             response = b"HTTP/1.1 404 Not Found\r\n\r\n"
         
